8bcf59ee2371d7e36c543eb02f85c825f4fe7a40
jeffrey.kim <jeffrey.kim@evite.com>
2026-04-22 08:55:28 -0700

    add: git-fugitive and icons to oil

--- plugin/oil.lua
+++ plugin/oil.lua
@@ -1,13 +1,16 @@
+vim.pack.add({ "https://github.com/nvim-mini/mini.nvim" })
 vim.pack.add({ "https://github.com/stevearc/oil.nvim" })
 
+require('mini.icons').setup({})
+
 require("oil").setup({
-	default_file_explorer = true,
-	columns = {
-		"icons",
-	},
-	view_options = {
-		show_hidden = true,
-	},
+  default_file_explorer = true,
+  columns = {
+    "icon",
+  },
+  view_options = {
+    show_hidden = true,
+  },
 })
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
