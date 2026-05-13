40e7bb7364b3f2604db11f6908182773bd147255
kimheffy <kimjeffrey96@gmail.com>
2026-05-12 21:47:25 -0700

    chore: add which-key maps

--- plugin/which-key.lua
+++ plugin/which-key.lua
@@ -1,12 +1,24 @@
 -- lazy loaded
 vim.schedule(function()
-  vim.pack.add({ "https://github.com/folke/which-key.nvim" })
+	vim.pack.add({ "https://github.com/folke/which-key.nvim" })
 
-  require("which-key").setup({
-    preset = "helix",
-  })
+	local wk = require("which-key")
 
-  vim.keymap.set("n", "<leader>?", function()
-    require("which-key").show({ global = false })
-  end, { desc = "Buffer Local Keymaps (which-key)" })
+	wk.setup({
+		preset = "helix",
+	})
+
+	wk.add({
+		{ "<leader>b", group = "[b]uffer" },
+		{ "<leader>c", group = "[c]onform" },
+		{ "<leader>f", group = "[f]zflua" },
+		{ "<leader>g", group = "diff[git]" },
+		{ "<leader>t", group = "[t]ab" },
+		{ "<leader>u", group = "[u]i" },
+		{ "<leader>x", group = "fi[x] list" },
+	})
+
+	vim.keymap.set("n", "<leader>?", function()
+		require("which-key").show({ global = false })
+	end, { desc = "Buffer Local Keymaps (which-key)" })
 end)
