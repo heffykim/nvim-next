fda4aefcd26245812183c60d28377d5ec0b1e8a7
Jeffrey Kim <jeffrey.kim@evite.com>
2026-05-13 17:51:53 -0700

    enhancements to nvim config (#2)
    * add: git-fugitive and icons to oil
    
    * style: reformat
    
    * add: lua config for ftplugin
    
    * add: fzf-lua git-status to keymap
    
    * add: minimal terminal plugin
    
    * fix: change lualine to absolute path
    
    * add: install arborist and org-mode

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -13,15 +13,61 @@ vim.lsp.config("*", {
 })
 
 vim.lsp.enable({
-	"lua_ls",
-	"ruff",
-	"pyrefly",
-	"isort",
-	"flake8",
-	"black",
-	"biome",
-	"vtsls",
+  "lua_ls",
+  "ruff",
+  "pyrefly",
+  "isort",
+  "flake8",
+  "black",
+  "biome",
+  "vtsls",
 	"rust-analyzer",
+  "org"
 })
 
 vim.diagnostic.config({ virtual_text = true })
+
+vim.keymap.set('n', 'grd', '<cmd>lua vim.lsp.buf.definition()<cr>', { desc = "go to definition" })
+
+
+
+
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('my.lsp', {}),
+  callback = function(ev)
+    local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+    if client:supports_method('textDocument/implementation') then
+      -- Create a keymap for vim.lsp.buf.implementation ...
+    end
+
+    -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
+    if client:supports_method('textDocument/completion') then
+      -- Optional: trigger autocompletion on EVERY keypress. May be slow!
+      -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+      -- client.server_capabilities.completionProvider.triggerCharacters = chars
+
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+    end
+
+    -- Auto-format ("lint") on save.
+    -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
+    -- Disabled: conform.nvim handles format-on-save (see plugin/formatter.lua).
+    -- Leaving both enabled caused LSP formatters (vtsls/biome LSP) to overwrite
+    -- conform's output, ignoring biome.json.
+    -- if not client:supports_method('textDocument/willSaveWaitUntil')
+    --     and client:supports_method('textDocument/formatting') then
+    --   vim.api.nvim_create_autocmd('BufWritePre', {
+    --     group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
+    --     buffer = ev.buf,
+    --     callback = function()
+    --       vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 1000 })
+    --     end,
+    --   })
+    -- end
+  end,
+})
+
+
+vim.opt.complete:append('o')
+-- vim.opt.completeopt = { 'menuone', 'noselect' }
